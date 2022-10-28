16ce862dea5bf7ca5d5198fdc27a0490473b535d
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-28 12:16:42 -0500

    Create a blogs array & map it with styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Blog from './components/Blog/Blog';
 import FaqSection from './components/Faq/FaqSection';
 import Hero from './components/Hero/Hero';
 import Layout from './components/Layouts/Layout';
@@ -13,6 +14,7 @@ function App() {
         <Services />
         <Testimonies />
         <FaqSection />
+        <Blog />
       </div>
     </Layout>
   );
