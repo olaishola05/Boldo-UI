fdc982bf3b03ed19f0d20d9f5d5f528fbac1de36
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-28 08:15:13 -0500

    Create new branch & component for testimonies

---
+++ src/components/Testimonies/Testimonies.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Testimonies.module.css';
+
+const Testimonies = () => (
+  <div className={styles.container}>Testimonies</div>
+);
+
+export default Testimonies;
