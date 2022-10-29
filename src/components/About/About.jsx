e6baaa42357f967c316bdc3ba354bfeecc48b034
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-29 15:09:39 -0500

    Complete the company story section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,6 +25,49 @@ const About = () => (
         <img src={fileImg} alt="Files" />
         <img src={groupImg} alt="Collaboration in group" />
       </div>
+
+      <div className={styles.story}>
+        <h2>Our story</h2>
+        <h3>Handshake infographic mass market crowdfunding iteration.</h3>
+        <p>
+          Conversion angel investor entrepreneur first mover advantage.
+          Handshake infographic mass market crowdfunding iteration. Traction
+          stock user experience deployment beta innovator incubator focus. Sales
+          user experience branding growth hacking holy grail monetization
+          conversion prototype stock network effects. Learning curve network
+          effects return on investment bootstrapping business-to-consumer.
+        </p>
+      </div>
+    </section>
+
+    <section className={styles.info}>
+      <div>
+        <h2>Our numbers</h2>
+        <h3>Handshake infographic mass market crowdfunding iteration.</h3>
+
+        <div className={styles.infographic}>
+          <div>
+            <h4>
+              120m
+            </h4>
+            <span>Cool feature title</span>
+          </div>
+          <div>
+
+            <h4>
+              10.000
+            </h4>
+            <span>Cool feature title</span>
+          </div>
+          <div>
+
+            <h4>
+              240
+            </h4>
+            <span>Cool feature title</span>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 );
