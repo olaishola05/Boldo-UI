7af560b7332eede89b280f3a93415e13b720cd8f
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-30 17:14:16 -0500

    Add a feature to dynamically change navbar bg & color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layouts/Layout';
 import Home from './components/Home/Home';
 import BlogHomepage from './components/Blog/BlogHomepage';
 import About from './components/About/About';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.title = 'Home';
+    }
+    if (location.pathname === '/blog') {
+      document.title = 'Blog';
+    }
+    if (location.pathname === '/about') {
+      document.title = 'About';
+    }
+  }, [location]);
+
   return (
     <Layout>
       <div className="App">
--- src/components/Layouts/TopBar.jsx
+++ src/components/Layouts/TopBar.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '../../images/Logo.png';
 import styles from './Layout.module.css';
 
 function TopBar() {
+  const [changeClass, setChangeClass] = useState(styles.navcontainer);
+  const [logo, setLogo] = useState(styles.logo);
   const links = [
     { to: '/', text: 'Product' },
     { to: '/#service', text: 'Service' },
     { to: '/about', text: 'About' },
     { to: '/login', text: 'Log In' },
   ];
-  const { hash, key } = useLocation();
+  const location = useLocation();
+  const { hash, key } = location;
 
   useEffect(() => {
     if (hash) {
@@ -19,9 +22,16 @@ function TopBar() {
     }
   }, [key, hash]);
 
+  useEffect(() => {
+    if (window.location.pathname === '/blog') {
+      setChangeClass(styles.dynamicNav);
+      setLogo(styles.dynamicLogo);
+    }
+  }, [location]);
+
   return (
-    <div className={styles.navcontainer}>
-      <div className={styles.logo}>
+    <div className={changeClass}>
+      <div className={logo}>
         <img src={Logo} alt="company logo" />
         <h1><Link to="/">Boldo</Link></h1>
       </div>
