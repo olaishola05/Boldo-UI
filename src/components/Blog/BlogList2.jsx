f1bf8f3792b3504e38b8e7f2708e0fa916534cb1
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-31 08:35:04 -0500

    Fix bug with blogs rendering

--- src/components/Blog/BlogList2.jsx
+++ src/components/Blog/BlogList2.jsx
@@ -1,48 +1,33 @@
 import React from 'react';
-import PropTypes, { shape } from 'prop-types';
 import styles from './Blog.module.css';
+import blogs from '../../utills/blogs';
 
-const BlogList2 = (props) => {
-  const { blogs } = props;
-  return (
-    <>
-      <div className={styles.blogs}>
-        {blogs.map((blog) => (
-          <div className={styles.blog} key={blog.id}>
-            <div className={styles.blogImg}>
-              <img src={blog.postImg} alt="blog" />
+const BlogList2 = () => (
+  <>
+    <div className={styles.blogs}>
+      {blogs.map((blog) => (
+        <div className={styles.blog} key={blog.id}>
+          <div className={styles.blogImg}>
+            <img src={blog.postImg} alt="blog" />
+          </div>
+          <div className={styles.blogContent}>
+            <div className={styles.content}>
+              <h4>{blog.title}</h4>
+              {' '}
+              <span>{blog.date}</span>
             </div>
-            <div className={styles.blogContent}>
-              <div className={styles.content}>
-                <h4>{blog.title}</h4>
-                {' '}
-                <span>{blog.date}</span>
-              </div>
-              <p className={styles.description}>{blog.content}</p>
-              <div className={styles.blogFooter}>
-                <div className={styles.postInfo}>
-                  <img src={blog.userImg} alt="" />
-                  <p>{blog.postBy}</p>
-                </div>
+            <p className={styles.description}>{blog.content}</p>
+            <div className={styles.blogFooter}>
+              <div className={styles.postInfo}>
+                <img src={blog.userImg} alt="" />
+                <p>{blog.postBy}</p>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    </>
-  );
-};
-
-BlogList2.propTypes = {
-  blogs: PropTypes.arrayOf(shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    postImg: PropTypes.string.isRequired,
-    postBy: PropTypes.string.isRequired,
-    userImg: PropTypes.string.isRequired,
-  })).isRequired,
-};
+        </div>
+      ))}
+    </div>
+  </>
+);
 
 export default BlogList2;
