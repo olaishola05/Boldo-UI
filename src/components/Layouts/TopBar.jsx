cc66f1f071cb1d3d38fd923b52ffe6290194955c
Ola Ishola <olaishola@hotmail.co.uk>
2022-11-02 16:51:15 +0100

    Create new branch for mobile responsiveness

--- src/components/Layouts/TopBar.jsx
+++ src/components/Layouts/TopBar.jsx
@@ -13,6 +13,7 @@ function TopBar() {
   const [logo, setLogo] = useState(styles.logo);
   const [brand, setBrand] = useState(styles.Logo);
   const { isTheme, toggleTheme } = React.useContext(ThemeContext);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const links = [
     { to: '/', text: 'Product' },
@@ -48,6 +49,22 @@ function TopBar() {
         <img src={isTheme ? blackLogo : brand} alt="company logo" />
         <h1><Link to="/" style={isTheme ? { backgroundColor: 'white', color: 'black' } : {}}>Boldo</Link></h1>
       </div>
+
+      <button type="button" className={styles.hamburger} onClick={() => setIsNavExpanded(!isNavExpanded)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+
+      </button>
 
       <ul>
         { links.map((link) => (
